961e0845bd338343b2cde09c587bce710fae5e77
Ramsay Shiells <ramsay.shiells@Ramsays-MBP.lan>
2020-03-22 11:18:41 +0000

    reduce padding in sections on mobile

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,6 +4,9 @@ import styled from "styled-components"
 const Container = styled.div`
   background-color: ${props => props.color};
   padding: 70px 50px;
+  @media (max-width: 600px) {
+    padding: 70px 25px;
+  }
   ... ${props => props.style};
 `
 
